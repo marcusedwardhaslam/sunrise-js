1405df0996335fdc6951c584f7d8beb588825e06
Marcus Haslam <marcusedwardhaslam@gmail.com>
2023-01-22 20:59:13 +0000

    Fix tests

--- src/meanSolarTime.spec.ts
+++ src/meanSolarTime.spec.ts
@@ -5,7 +5,7 @@ describe('when using the meanSolarTime module', () => {
     it('should return the correct julian time for the unix epoch at the meridian', () => {
       const unixTimestamp = new Date('1970-01-01 13:00:00');
       const result = meanSolarTime(0, unixTimestamp.getTime() / 1000);
-      expect(result).toBeCloseTo(2440588);
+      expect(result).toBeCloseTo(2440588, 1);
     });
 
     it('should return the correct julian time for the 21st January 2023 at the meridian', () => {
